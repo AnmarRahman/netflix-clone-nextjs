0908022d0c8bce559e9b08fcc6654a8bd248c473
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-03-14 18:22:48 -0400

    installed text shadow npm for tailwindcss

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,6 @@ const Home = ({ netflixOriginals,
   topRated,
   trendingNow,
 }: Props) => {
-  console.log(netflixOriginals)
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
