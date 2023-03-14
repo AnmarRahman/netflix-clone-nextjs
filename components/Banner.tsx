c23687381a21da1547a559faf9d2c6499f22e09c
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-03-14 12:16:16 -0400

    generate random movie for banner component

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,16 +1,23 @@
 import Image from "next/image"
 import { Movie } from "../typings"
+import { useEffect, useState } from "react"
 
 interface Props {
     netflixOriginals: Movie[]
 }
 
 function Banner({ netflixOriginals }: Props) {
+    const [movie, setMovie] = useState<Movie | null>(null)
+
+    useEffect(() => {
+        setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)])
+    }, [netflixOriginals])
+
     return (
         <div>
-            {/* <div>
-                <Image />
-            </div> */}
+            <div>
+                {/* <Image src={}/> */}
+            </div>
         </div>
     )
 }
