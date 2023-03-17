c2d543e75ab27749565a81aa05ec9ee01f229b5a
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-03-16 23:30:11 -0400

    recoil js modal state + modal working with banner button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
-import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { useRecoilValue } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Modal from '../components/Modal'
 import Row from '../components/Row'
 import useAuth from '../hooks/useAuth'
 import { Movie } from '../typings'
@@ -28,7 +29,8 @@ const Home = ({ netflixOriginals,
   topRated,
   trendingNow,
 }: Props) => {
-  const { logout, loading } = useAuth()
+  const { loading } = useAuth()
+  const showModal = useRecoilValue(modalState)
 
   if (loading) return null
 
@@ -52,7 +54,7 @@ const Home = ({ netflixOriginals,
           <Row title="Documentaries" movies={documentaries} />
         </section>
       </main>
-      {/* Modal */}
+      {showModal && <Modal />}
     </div>
   )
 }
