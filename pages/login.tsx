1ecb76f34ae42aacf4acd90cf5669b453a02b8cc
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-03-16 20:49:46 -0400

    login minor change in onSubmit function

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,8 @@
-import Head from "next/head"
-import Image from "next/image"
-import { useState } from "react"
-import { useForm } from "react-hook-form"
-import { SubmitHandler } from "react-hook-form/dist/types"
+import Head from 'next/head'
+import Image from 'next/image'
+import { useRef, useState } from 'react'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import useAuth from '../hooks/useAuth'
 
 interface Inputs {
     email: string
@@ -11,8 +11,16 @@ interface Inputs {
 
 function Login() {
     const [login, setLogin] = useState(false)
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
+    const { signIn, signUp } = useAuth()
+
+    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+    const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
+        if (login) {
+            await signIn(email, password)
+        } else {
+            await signUp(email, password)
+        }
+    };
 
 
     return (
@@ -37,19 +45,26 @@ function Login() {
                 <div className="space-y-4">
                     <label className="inline-block w-full">
                         <input type="email" placeholder="Email" className="input" {...register("email", { required: true })} />
+                        {errors.email && <p className="p-1 text-[13px] font-light  text-orange-500">
+                            Please enter a valid email.
+                        </p>}
                     </label>
                     <label className="inline-block w-full">
                         <input type="password" placeholder="Password" className="input" {...register("password", { required: true })} />
+                        {errors.password && <p className="p-1 text-[13px] font-light  text-orange-500">
+                            Your password must contain between 4 and 60 characters.
+                        </p>}
+
                     </label>
                 </div>
 
-                <button className="w-full rounded bg-[#E50914] py-3 font-semibold">
+                <button className="w-full rounded bg-[#E50914] py-3 font-semibold" onClick={() => setLogin(true)}>
                     Sign In
                 </button>
 
                 <div className="text-[gray]">
                     New to Netflix?{' '}
-                    <button type="submit" className="cursor-pointer text-white hover:underline">
+                    <button type="submit" className="cursor-pointer text-white hover:underline" onClick={() => setLogin(false)}>
                         Sign up now
                     </button>
                 </div>
