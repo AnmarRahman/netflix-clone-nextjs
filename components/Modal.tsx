3c21ad41dc824d2779d5a3236f06f8e13d4980e6
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-03-17 10:43:38 -0400

    useEffect for trailers on modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,9 +1,65 @@
-import MuiModal from "@mui/material/Modal"
-import { useRecoilState, useRecoilValue } from "recoil"
-import { modalState } from "../atoms/modalAtom"
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom'
+import ReactPlayer from 'react-player/lazy'
+import { FaPlay } from 'react-icons/fa'
+import {
+    CheckIcon,
+    PlusIcon,
+    ThumbUpIcon,
+    VolumeOffIcon,
+    VolumeUpIcon,
+    XIcon,
+} from '@heroicons/react/outline'
+import { Element, Genre, Movie } from '../typings'
+import MuiModal from '@mui/material/Modal'
+import {
+    collection,
+    deleteDoc,
+    doc,
+    DocumentData,
+    onSnapshot,
+    setDoc,
+} from 'firebase/firestore'
+import { db } from '../firebase'
+import useAuth from '../hooks/useAuth'
+
 
 function Modal() {
+    const [movie, setMovie] = useRecoilState(movieState)
+    const [trailer, setTrailer] = useState('')
     const [showModal, setShowModal] = useRecoilState(modalState)
+    const [muted, setMuted] = useState(false)
+    const [genres, setGenres] = useState<Genre[]>([])
+    const [addedToList, setAddedToList] = useState(false)
+    const { user } = useAuth()
+    const [movies, setMovies] = useState<DocumentData[] | Movie[]>([])
+
+
+
+    useEffect(() => {
+        if (!movie) return
+
+        async function fetchMovie() {
+            const data = await fetch(
+                `https://api.themoviedb.org/3/${movie?.media_type === 'tv' ? 'tv' : 'movie'
+                }/${movie?.id}?api_key=${process.env.NEXT_PUBLIC_API_KEY
+                }&language=en-US&append_to_response=videos`
+            ).then((response) => response.json())
+
+            if (data?.videos) {
+                const index = data.videos.results.findIndex(
+                    (element: Element) => element.type === 'Trailer'
+                )
+                setTrailer(data.videos?.results[index]?.key)
+            }
+
+            if (data?.genres) {
+                setGenres(data.genres)
+            }
+        }
+        fetchMovie()
+    }, [movie])
 
     const handleClose = () => {
         setShowModal(false)
@@ -12,7 +68,20 @@ function Modal() {
     return (
         <MuiModal open={showModal} onClose={handleClose}>
             <>
-                Modal
+                <button onClick={handleClose} className="modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]">
+                    <XIcon className="h-6 w-6" />
+                </button>
+
+                <div className='relative pt-[56.25%] '>
+                    <ReactPlayer
+                        url={`https://www.youtube.com/watch?v=${trailer}`}
+                        width="100%"
+                        height="100%"
+                        style={{ position: 'absolute', top: '0', left: '0' }}
+                        playing
+                        muted={muted}
+                    />
+                </div>
             </>
         </MuiModal>
     )
