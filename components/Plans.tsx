b65dc52ad78ae8d54e193bd9f45058cba8ec1da0
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-03-18 22:58:01 -0400

    bug fixed, had to install next transpile modules

--- components/Plans.tsx
+++ components/Plans.tsx
@@ -53,15 +53,7 @@ function Plans() {
 
                 <div className="mt-4 flex flex-col space-y-4">
                     <div className="flex w-full items-center justify-center self-end md:w-3/5">
-                        <div className="planBox">
-                            standard
-                        </div>
-                        <div className="planBox">
-                            standard
-                        </div>
-                        <div className="planBox">
-                            standard
-                        </div>
+                        { }
                     </div>
 
                     {/* <Table /> */}
--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withTM = require("next-transpile-modules")([
+  "@stripe/firestore-stripe-payments",
+]); // pass the modules you would like to see transpiled
+
+module.exports = withTM({
   reactStrictMode: true,
   images: {
     remotePatterns: [
@@ -13,4 +18,4 @@ module.exports = {
       },
     ],
   },
-};
+});
