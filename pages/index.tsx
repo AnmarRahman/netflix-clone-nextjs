3d2f5e8ddcb5ed8e066fc921250927c9dae19e7a
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-03-18 22:03:57 -0400

    plans page in progress. Plan styling for 3 plans

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { modalState } from '../atoms/modalAtom'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
 import Modal from '../components/Modal'
+import Plans from '../components/Plans'
 import Row from '../components/Row'
 import useAuth from '../hooks/useAuth'
 import { Movie } from '../typings'
@@ -31,8 +32,13 @@ const Home = ({ netflixOriginals,
 }: Props) => {
   const { loading } = useAuth()
   const showModal = useRecoilValue(modalState)
+  const subscription = false
 
-  if (loading) return null
+  if (loading || subscription === null) return null
+
+  if (!subscription) return (
+    <Plans />
+  )
 
   return (
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
