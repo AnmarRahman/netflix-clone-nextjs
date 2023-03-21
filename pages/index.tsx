83167c3182dab0a830d6619f72b5f60dd028057b
AnmaritoS <95002766+AnmaritoS@users.noreply.github.com>
2023-03-20 20:58:12 -0400

    manage plan and cancel plan plus account page styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ const Home = ({ netflixOriginals,
     <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/netflix.ico" />
       </Head>
       <Header />
       <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
